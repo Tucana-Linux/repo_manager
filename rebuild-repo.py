53ad5decca3c58835a345f1e261cc6724e0abafa
Rahul Chandra <rahul@projecteclipse.org>
2025-07-03 20:35:46 -0400

    Add main function

--- rebuild-repo.py
+++ rebuild-repo.py
@@ -134,36 +134,31 @@ def validate_state(state: dict[str, dict[str, Any]]) -> bool:
             print(f"ERROR Repo State validation failed, {package_name} requires {list(missing_dependencies)} {list(missing_make_dependencies)} which could not be found")
             return False
     return True
+def main() -> None:
+    repo = RepoManager(os.getcwd())
+    packages :  dict[str, dict[str, Any]] = repo.get_current_state()
+    packages_with_updates : list[str] = repo.get_updates()
+    print(f"Packages with updates {packages_with_updates}")
 
-repo = RepoManager(os.getcwd())
-packages :  dict[str, dict[str, Any]] = repo.get_current_state()
-packages_with_updates : list[str] = repo.get_updates()
-print(f"Packages with updates {packages_with_updates}")
+    for package in packages_with_updates:
+        print(f"Working on {package}")
+        package_dict = {}
+        current_version : str = ""
+        if package in packages:
+            current_version = packages[package]["version"]
+        package_dict["version"] = repo.extract_and_process_version(package, current_version)
+        package_dict["depends"] = repo.extract_depends(package)
+        package_dict["make_depends"] = repo.extract_make_depends(package)
+        package_dict["install_size"] = repo.get_install_size(package)
+        package_dict["download_size"] = repo.get_download_size(package)
+        package_dict["last_update"] = repo.get_last_update(package)
+        packages[package] = package_dict
+        repo.write_file_list(package)
 
-for package in packages_with_updates:
-    print(f"Working on {package}")
-    package_dict = {}
-    current_version : str = ""
-    if package in packages:
-        current_version = packages[package]["version"]
-    package_dict["version"] = repo.extract_and_process_version(package, current_version)
-    package_dict["depends"] = repo.extract_depends(package)
-    package_dict["make_depends"] = repo.extract_make_depends(package)
-    package_dict["install_size"] = repo.get_install_size(package)
-    package_dict["download_size"] = repo.get_download_size(package)
-    package_dict["last_update"] = repo.get_last_update(package)
-    packages[package] = package_dict
-    repo.write_file_list(package)
-
-print("Validating Repo State...")
-if validate_state(packages):
-    print("Writing Repo...")
-    repo.write_repo(packages)
+    print("Validating Repo State...")
+    if validate_state(packages):
+        print("Writing Repo...")
+        repo.write_repo(packages)
 
  
-    
-
-    
-
-
-
+main()
